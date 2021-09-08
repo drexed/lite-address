80b5d8532ee636b88e3ba50d3990fb4b8181557c
Juan Gomez <j.gomez@drexed.com>
2021-09-08 12:02:55 -0400

    Improve address details

--- lib/lite/address/details.rb
+++ lib/lite/address/details.rb
@@ -2,120 +2,91 @@
 
 module Lite
   module Address
-    class Details
-      attr_accessor(
-        :number,
-        :street,
-        :street_type,
-        :unit,
-        :unit_prefix,
-        :suffix,
-        :prefix,
-        :city,
-        :state,
-        :postal_code,
-        :postal_code_ext,
-        :street2,
-        :street_type2,
-        :suffix2,
-        :prefix2,
-        :redundant_street_type
-      )
 
-      def initialize(args)
-        args.each do |attr, val|
-          public_send("#{attr}=", val)
-        end
-      end
+    DETAIL_KEYS = %i[
+      number street street_type unit unit_prefix suffix prefix city
+      state postal_code postal_code_ext street2 street_type2 suffix2
+      prefix2 redundant_street_type
+    ].freeze
 
+    class Details < Struct.new(*DETAIL_KEYS, keyword_init: true)
 
       def full_postal_code
-        return nil unless self.postal_code
-        self.postal_code_ext ? "#{postal_code}-#{postal_code_ext}" : self.postal_code
-      end
+        return if postal_code.nil?
 
+        [postal_code, postal_code_ext].compact.join('-')
+      end
 
       def state_fips
-        StreetAddress::US::FIPS_STATES[state]
+        Lite::Address::US::FIPS_STATES[state]
       end
-
 
       def state_name
-        name = StreetAddress::US::STATE_NAMES[state] and name.capitalize
+        Lite::Address::US::STATE_NAMES[state]&.capitalize
       end
-
 
       def intersection?
         !street2.nil?
       end
 
-
-      def line1(s = "")
-        s = s.dup
-        parts = []
-        if intersection?
-          parts << prefix       if prefix
-          parts << street
-          parts << street_type  if street_type
-          parts << suffix       if suffix
-          parts << 'and'
-          parts << prefix2      if prefix2
-          parts << street2
-          parts << street_type2 if street_type2
-          parts << suffix2      if suffix2
-        else
-          parts << number
-          parts << prefix if prefix
-          parts << street if street
-          parts << street_type if street_type && !redundant_street_type
-          parts << suffix if suffix
-          parts << unit_prefix if unit_prefix
-          #follow guidelines: http://pe.usps.gov/cpim/ftp/pubs/Pub28/pub28.pdf pg28
-          parts << (unit_prefix ? unit : "\# #{unit}") if unit
-        end
-        s + parts.join(' ').strip
+      def line1(str = String.new)
+        parts = intersection? ? intersection_line1 : address_line1
+        str + parts.compact.join(' ').strip
       end
 
-
-      def line2(s = "")
-        s = s.dup
-        parts = []
-        parts << city  if city
-        parts << state if state
-        s = s + parts.join(', ')
-        if postal_code
-          s << " #{postal_code}"
-          s << "-#{postal_code_ext}" if postal_code_ext
-        end
-        s.strip
+      def line2(str = String.new)
+        str += [city, state].compact.join(', ')
+        str << " #{full_postal_code}" if postal_code
+        str.strip
       end
-
-
-      def to_s(format = :default)
-        s = "".dup
-        case format
-        when :line1
-          s << line1(s)
-        when :line2
-          s << line2(s)
-        else
-          s << [line1, line2].select{ |l| !l.empty? }.join(', ')
-        end
-        s
-      end
-
 
       def to_h
-        self.instance_variables.each_with_object({}) do |var_name, hash|
-          var_value = self.instance_variable_get(var_name)
-          hash_name = var_name[1..-1].to_sym
-          hash[hash_name] = var_value
+        Lite::Address::DETAIL_KEYS.each_with_object({}) do |key, hash|
+          hash[key] = public_send(key)
+        end
+      end
+
+      def to_s(format = :default)
+        case format
+        when :line1 then line1
+        when :line2 then line2
+        else [line1, line2].reject(&:empty?).join(', ')
         end
       end
 
       def ==(other)
         to_s == other.to_s
       end
+
+      private
+
+      def address_line1
+        parts = []
+        parts << number
+        parts << prefix
+        parts << street
+        parts << street_type unless redundant_street_type
+        parts << suffix
+        parts << unit_prefix
+        # follow guidelines: http://pe.usps.gov/cpim/ftp/pubs/Pub28/pub28.pdf pg28
+        parts << (unit_prefix ? unit : "\# #{unit}") if unit
+        parts
+      end
+
+      def intersection_line1
+        parts = []
+        parts << prefix
+        parts << street
+        parts << street_type
+        parts << suffix
+        parts << 'and'
+        parts << prefix2
+        parts << street2
+        parts << street_type2
+        parts << suffix2
+        parts
+      end
+
     end
   end
 end
